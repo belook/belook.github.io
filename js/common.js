8a3e690a271ec41c7b77e1865a10dd8c65994e5b
belook.github.io <serhiybilik@gmail.com>
2017-10-05 13:51:56 +0300

    1

--- js/common.js
+++ js/common.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function () {
 		return false;
 	});
         jQuery(document).ready(function() {
-jQuery('.post').addClass("hidden").viewportChecker({
+jQuery('section').addClass("hidden").viewportChecker({
 classToAdd: 'visible animated fadeIn',
 offset: 100
 });
